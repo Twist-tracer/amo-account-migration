82706fbb90c4eec36b4bd669691fa53b0d5dbeb0
sbogdanov <sbogdanov@team.amocrm.com>
2018-07-16 12:17:51 +0300

    removed comment

--- config.php
+++ config.php
@@ -1,24 +1,6 @@
 <?php
 
 return [
-//	'from' => [
-//		'account' => [
-//			'USER_LOGIN' => '',
-//			'USER_HASH' => ''
-//		],
-//		'protocol' => 'https://',
-//		'subdomain' => '',
-//		'domain' => 'amocrm.com',
-//	],
-//	'to' => [
-//		'account' => [
-//			'USER_LOGIN' => '',
-//			'USER_HASH' => ''
-//		],
-//		'protocol' => 'https://',
-//		'subdomain' => '',
-//		'domain' => 'amocrm.ru',
-//	],
 	'from' => [
 		'account' => [
 			'USER_LOGIN' => '',
